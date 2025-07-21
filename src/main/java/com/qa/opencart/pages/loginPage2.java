f7ea79c838f665a2378b59b6d6f1cc2f42a47f10
TejaswiniChothe <tejaswini.chothe11@gmail.com>
2025-07-21 09:21:26 +0530

    push login page2 modified chnages

--- src/main/java/com/qa/opencart/pages/loginPage2.java
+++ src/main/java/com/qa/opencart/pages/loginPage2.java
@@ -3,7 +3,7 @@ package com.qa.opencart.pages;
 import org.openqa.selenium.By;
 
 public class loginPage2 {
-	 private By email=By.id("button");
+	 private By email_ID=By.id("button");
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      
