1b57091d4827cc4509907b684952f5d3800f5d29
TejaswiniChothe <tejaswini.chothe11@gmail.com>
2025-07-22 11:03:56 +0530

    Employee page chnages

---
+++ src/main/java/com/qa/opencart/pages/EmployeePage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class EmployeePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi I am Tej");
+		System.out.println("Hi I am Ash");
+
+	}
+
+}
