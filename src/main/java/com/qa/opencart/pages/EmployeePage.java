33c005c9ee1ba1d74847d82f547e145d86e18e17
TejaswiniChothe <59131575+TejaswiniChothe@users.noreply.github.com>
2025-07-22 09:38:34 +0530

    Update EmployeePage.java

--- src/main/java/com/qa/opencart/pages/EmployeePage.java
+++ src/main/java/com/qa/opencart/pages/EmployeePage.java
@@ -5,7 +5,6 @@ public class EmployeePage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi I am Tej");
-		System.out.println("Hi I am Ash");
 
 	}
 
